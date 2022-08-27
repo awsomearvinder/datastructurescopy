091caaa8092afd9e18add4f493c3ceeee9d5d65a
Arvinder <ArvinderDhan@gmail.com>
2022-08-27 16:20:49 -0500

    use eachDefaultSystem

--- flake.nix
+++ flake.nix
@@ -4,32 +4,35 @@
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
   inputs.alejandra.url = "github:kamadorueda/alejandra/3.0.0";
   inputs.alejandra.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.flake-utils.inputs.nixpkgs.follows = "nixpkgs";
 
   outputs = {
     self,
     nixpkgs,
     alejandra,
-  }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
-    };
-  in {
-    devShell.${system} = pkgs.mkShell rec {
-      name = "java-shell";
-      packages = [
-        alejandra.defaultPackage.${system}
-      ];
-      buildInputs = with pkgs; [
-        jdk17
-        jetbrains.idea-ultimate
-      ];
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in {
+      devShell = pkgs.mkShell rec {
+        name = "java-shell";
+        packages = [
+          alejandra.defaultPackage.${system}
+        ];
+        buildInputs = with pkgs; [
+          jdk17
+          jetbrains.idea-ultimate
+        ];
 
-      shellHook = ''
-        export _JAVA_AWT_WM_NONREPARENTING=1
-        export PATH="${pkgs.jdk17}/bin:$PATH"
-      '';
-    };
-  };
+        shellHook = ''
+          export _JAVA_AWT_WM_NONREPARENTING=1
+          export PATH="${pkgs.jdk17}/bin:$PATH"
+        '';
+      };
+    });
 }
